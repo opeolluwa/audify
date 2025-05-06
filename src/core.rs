7a704a62528da84b9daacc7b43eda3ce0f6ef4b5
Adeoye Adefemi <adefemiadeoye@yahoo.com>
2025-05-06 22:53:42 +0100

    tts works

--- src/core.rs
+++ src/core.rs
@@ -1,6 +1,7 @@
-use serde::{Deserialize, Serialize};
-
 use crate::{error::SynthesisError, languages::Languages};
+use piper_rs::synth::PiperSpeechSynthesizer;
+use serde::{Deserialize, Serialize};
+use std::path::Path;
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Audify {
@@ -8,15 +9,19 @@ pub struct Audify {
     model_path: String,
     export_path: String,
     source: String,
+    config_path: String,
+    sid: i64,
 }
 
 impl Default for Audify {
     fn default() -> Self {
         Self {
             language: Default::default(),
-            model_path: Default::default(),
-            export_path: Default::default(),
+            model_path: "en_US-libritts_r-medium.onnx.json".to_string(),
+            config_path: "en_US-libritts_r-medium.onnx.json".to_string(),
+            export_path: ".".to_string(),
             source: Default::default(),
+            sid: 50i64,
         }
     }
 }
@@ -30,7 +35,17 @@ impl Audify {
     }
 
     // source: source.to_string()
-    pub fn synthesize(source: &str) -> Result<(), SynthesisError> {
+    pub fn synthesize(&self, source: &str) -> Result<(), SynthesisError> {
+        let text = source.to_string();
+
+        let model = piper_rs::from_config_path(Path::new(&self.config_path)).unwrap();
+
+        model.set_speaker(self.sid);
+
+        let synth = PiperSpeechSynthesizer::new(model).unwrap();
+        synth
+            .synthesize_to_file(Path::new(&self.export_path), text, None)
+            .unwrap();
         Ok(())
     }
 }
